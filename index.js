993e4cae6277de56bf6458b9a27e237b48fff28b
mehdirazazaid <161901020+mehdirazazaid@users.noreply.github.com>
2024-03-05 05:21:20 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+// Example String Data Type with concatenation
+
+var str1 = "Hello";
+var str2 = "World";
+var concatenatedString = str1 + " " + str2;
+console.log("Concatenated String:", concatenatedString);
+
+// Example Number Data Type with arithmetic operations like add, subtraction, multiplication, etc
+
+var num1 = 10;
+var num2 = 5;
+var addition = num1 + num2;
+var subtraction = num1 - num2;
+var multiplication = num1 * num2;
+var division = num1 / num2;
+console.log("Addition:", addition);
+console.log("Subtraction:", subtraction);
+console.log("Multiplication:", multiplication);
+console.log("Division:", division);
+
+// Example Boolean Data Type
+
+var isTrue = true;
+var isFalse = false;
+console.log("isTrue:", isTrue);
+console.log("isFalse:", isFalse);
+
+// Example null Data Type
+
+var nullValue = null;
+console.log("Null Value:", nullValue);
+
+// Example undefined Data Type
+
+var undefinedValue = undefined;
+console.log("Undefined Value:", undefinedValue);
+
+// Example symbol Data Type
+
+var sym1 = Symbol("key1");
+var sym2 = Symbol("key2");
+console.log("Symbol 1:", sym1);
+console.log("Symbol 2:", sym2);
+
+// Example bigint Data Type
+
+var bigIntValue = 9007199254740991n;
+console.log("Big Int Value:", bigIntValue);
---
+++ index.ts
@@ -0,0 +1,48 @@
+// Example String Data Type with concatenation
+
+let str1: string = "Hello";
+let str2: string = "World";
+let concatenatedString: string = str1 + " " + str2;
+console.log("Concatenated String:", concatenatedString);
+
+// Example Number Data Type with arithmetic operations like add, subtraction, multiplication, etc
+
+let num1: number = 10;
+let num2: number = 5;
+let addition: number = num1 + num2;
+let subtraction: number = num1 - num2;
+let multiplication: number = num1 * num2;
+let division: number = num1 / num2;
+console.log("Addition:", addition);
+console.log("Subtraction:", subtraction);
+console.log("Multiplication:", multiplication);
+console.log("Division:", division);
+
+// Example Boolean Data Type
+
+let isTrue: boolean = true;
+let isFalse: boolean = false;
+console.log("isTrue:", isTrue);
+console.log("isFalse:", isFalse);
+
+// Example null Data Type
+
+let nullValue: null = null;
+console.log("Null Value:", nullValue);
+
+// Example undefined Data Type
+
+let undefinedValue: undefined = undefined;
+console.log("Undefined Value:", undefinedValue);
+
+// Example symbol Data Type
+
+let sym1: symbol = Symbol("key1");
+let sym2: symbol = Symbol("key2");
+console.log("Symbol 1:", sym1);
+console.log("Symbol 2:", sym2);
+
+// Example bigint Data Type
+
+let bigIntValue: bigint = 9007199254740991n;
+console.log("Big Int Value:", bigIntValue);
